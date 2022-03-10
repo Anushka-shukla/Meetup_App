04cc51ffb61cc50d3a66bc34b84cb52851f33d7a
Anushka-shukla <shuklaannushka@gmail.com>
2022-03-10 18:07:41 +0530

    add favorite meetup

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
+import { FavoritesContextProvider } from './components/store/favourites-context';
 
 ReactDOM.render(
+    <FavoritesContextProvider> 
     <BrowserRouter>
         <App />
-    </BrowserRouter>,
+    </BrowserRouter>
+    </FavoritesContextProvider>,
     document.getElementById('root')
 );
